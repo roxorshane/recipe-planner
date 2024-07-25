dd79f1447a4d681be296a569cb92881a64252482
Shane Reidy <mr.shanereidy@gmail.com>
2024-07-25 22:44:28 +0100

    Move AddRecipe & ShoppingList UI to PrimaryLayout
    * Adds ActionItems to TopAppBar

--- src/layouts/PrimaryLayout/PrimaryLayout.js
+++ src/layouts/PrimaryLayout/PrimaryLayout.js
@@ -1,21 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import { TopAppBar, TopAppBarRow, TopAppBarSection, TopAppBarTitle, TopAppBarFixedAdjust } from '@rmwc/top-app-bar';
-
+import { TopAppBar, TopAppBarActionItem, TopAppBarRow, TopAppBarSection, TopAppBarTitle, TopAppBarFixedAdjust } from 'rmwc';
+import { AddRecipeForm, ShoppingList } from 'components';
 import styles from './PrimaryLayout.module.scss';
 
+const panelContentType = {
+  none: 0,
+  addRecipeForm: 1,
+  shoppingList: 2
+};
+
 const PrimaryLayout = () => {
+  const [panelContent, setPanelContent] = useState(panelContentType.none);
+
+  const PanelContent = () => {
+    switch (panelContent) {
+      case panelContentType.addRecipeForm:
+        return <AddRecipeForm onAdd={() => togglePanelContent(panelContentType.none)} />;
+      case panelContentType.shoppingList:
+        return <ShoppingList />;
+      case panelContentType.none:
+      default:
+        return null;
+    }
+  };
+
+  const togglePanelContent = (contentType) => {
+    setPanelContent(
+      panelContent === contentType ? panelContentType.none : contentType
+    );
+  }
+
   return <div className={styles.PrimaryLayout}>
     <TopAppBar fixed>
       <TopAppBarRow>
         <TopAppBarSection>
-          <TopAppBarTitle className={styles.Title}>
+          <TopAppBarTitle>
             <span>Recipe Planner</span>
           </TopAppBarTitle>
+        </TopAppBarSection>
+        <TopAppBarSection alignEnd className={styles.ActionSection}>
+          <TopAppBarActionItem 
+              icon='library_add'
+              onClick={() => {
+                togglePanelContent(panelContentType.addRecipeForm)
+              }}
+          />
+          <TopAppBarActionItem 
+              icon='shopping_basket'
+              onClick={() => {
+                togglePanelContent(panelContentType.shoppingList)
+              }}
+          />
+          <div style={{height: "1px", width: "10px"}} />
         </TopAppBarSection>
       </TopAppBarRow>
     </TopAppBar>
     <TopAppBarFixedAdjust />
+    <PanelContent />
     <Outlet />
   </div>;
 };
--- src/views/DashboardView/DashboardView.js
+++ src/views/DashboardView/DashboardView.js
@@ -1,88 +1,21 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Button, Elevation, Grid, GridCell, TextField } from 'rmwc';
-import { RecipeList, FormulaInput, RecipeIngredientsList, ValidationMessage } from 'components';
-import { addRecipe } from 'data/slices/RecipeSlice';
-import { parseFormula, validateRecipe } from 'helpers';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Elevation, Grid, GridCell } from 'rmwc';
+import { RecipeList } from 'components';
 
 import styles from './DashboardView.module.scss';
 
 const DashboardView = () => {
-  const dispatch = useDispatch();
   const recipeLibrary = useSelector(state => state.recipeLibrary);
-  const [shoppingListIngredients, setShoppingListIngredients] = useState([]);
-  const [validationErrors, setValidationErrors] = useState();
-  const [nameInputInvalid, setNameInputInvalid] = useState();
-  const [formulaValue, setFormulaValue] = useState('');
-  const nameInput = useRef();
-
-  const handleFormulaInputChange = (value) => {
-    setFormulaValue(value);
-  };
-
-  const handleAddRecipeClick = () => {
-    const recipeName = nameInput.current.value;
-    if (!recipeName) { setNameInputInvalid(true); }
-
-    const recipeComponents = parseFormula(formulaValue);
-    validateRecipe({ recipeName, recipeComponents, recipeLibrary }).then(() => {
-      setValidationErrors(null);
-      dispatch(addRecipe({
-        name: recipeName,
-        ingredients: recipeComponents
-      }));
-    }).catch(setValidationErrors);
-  };
-
-  const updateShoppingList = () => {
-    if (!recipeLibrary?.length) { return; }
-    let shoppingList = {};
-    recipeLibrary.forEach(recipe => {
-      recipe.ingredients.forEach(ingredient => {
-        if (shoppingList[ingredient.name]) {
-          shoppingList[ingredient.name].quantity += Number(ingredient.quantity);
-        } else {
-          shoppingList[ingredient.name] = { ...ingredient };
-        }
-      });
-    });
-    setShoppingListIngredients(Object.values(shoppingList));
-  };
-  useEffect(updateShoppingList, [recipeLibrary]);
 
   return <div className={styles.DashboardView}>
     <Grid className={styles.Grid}>
       <GridCell span={5} desktop={5} phone={4}>
         <Elevation z={2} className={styles.AddRecipeContainer}>
-          <div className={styles.AddRecipeTitle}>Add a recipe</div>
-          <TextField 
-              outlined
-              fullwidth 
-              placeholder="Recipe name"
-              inputRef={nameInput}
-              invalid={nameInputInvalid}
-              onChange={() => setNameInputInvalid(false)}
-          />
-          <FormulaInput 
-              onChange={handleFormulaInputChange}
-              placeholderText="Recipe formula"
-          />
-          <ValidationMessage validationErrors={validationErrors} />
-          <Button 
-              unelevated
-              label="Add recipe"
-              onClick={handleAddRecipeClick}
-          />
           <div className={styles.RecipeLibraryContainer}>
-            <div className={styles.RecipeLibraryTitle}>Recipe Library</div>
+            <div className={styles.RecipeLibraryTitle}>Your recipes</div>
             <RecipeList recipes={recipeLibrary} />
           </div>
-        </Elevation>
-      </GridCell>
-      <GridCell span={4} desktop={4} phone={4}>
-        <Elevation z={2} className={styles.ShoppingListContainer}>
-          <div className={styles.ShoppingListTitle}>Shopping List</div>
-          <RecipeIngredientsList ingredients={shoppingListIngredients} />
         </Elevation>
       </GridCell>
     </Grid>
