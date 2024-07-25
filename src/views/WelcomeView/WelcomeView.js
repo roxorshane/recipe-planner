cb7fda689d9933786b07af9b6ff7ca22b2f6a2ef
Shane Reidy <mr.shanereidy@gmail.com>
2024-07-25 22:42:11 +0100

    Add WelcomeView view

---
+++ src/views/WelcomeView/WelcomeView.js
@@ -0,0 +1,12 @@
+import styles from './WelcomeView.module.scss';
+
+const WelcomeView = () => {
+  return <div className={styles.WelcomeView}>
+    "Welcome View"
+  </div>;
+};
+
+export {
+  WelcomeView
+};
+export default WelcomeView;
