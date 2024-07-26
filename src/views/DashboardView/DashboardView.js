40867dec5d09b0cadeab84feba11afb4cbd817c4
Shane Reidy <mr.shanereidy@gmail.com>
2024-07-26 10:29:02 +0100

    Add empty state to DashboardView

--- src/views/DashboardView/DashboardView.js
+++ src/views/DashboardView/DashboardView.js
@@ -2,22 +2,35 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Elevation, Grid, GridCell } from 'rmwc';
 import { RecipeList } from 'components';
+import { EmptyState } from './EmptyState.js';
 
 import styles from './DashboardView.module.scss';
 
 const DashboardView = () => {
   const recipeLibrary = useSelector(state => state.recipeLibrary);
 
+  const shouldShowEmptyState = () => {
+    return !recipeLibrary?.length;
+  }
+
+  const Content = () => {
+    if (shouldShowEmptyState()) { 
+      return <EmptyState styles={styles} />;
+    }
+
+    return <Elevation z={2} className={styles.RecipeLibraryContainer}>
+      <div className={styles.RecipeLibraryTitle}>Your recipes</div>
+      <RecipeList recipes={recipeLibrary} />
+    </Elevation>;
+  };
+
   return <div className={styles.DashboardView}>
     <Grid className={styles.Grid}>
-      <GridCell span={5} desktop={5} phone={4}>
-        <Elevation z={2} className={styles.AddRecipeContainer}>
-          <div className={styles.RecipeLibraryContainer}>
-            <div className={styles.RecipeLibraryTitle}>Your recipes</div>
-            <RecipeList recipes={recipeLibrary} />
-          </div>
-        </Elevation>
+      <GridCell desktop={3} />
+      <GridCell span={5} desktop={6} phone={4}>
+        <Content />
       </GridCell>
+      <GridCell desktop={3} />
     </Grid>
   </div>;
 };
---
+++ src/views/DashboardView/EmptyState.js
@@ -0,0 +1,27 @@
+import { IconButton } from 'rmwc';
+
+import bakingIllustration from 'assets/baking_illustration.jpg';
+
+const EmptyState = ({ styles }) => {
+  return <div className={styles.EmptyStateContainer}>
+    <div className={styles.Title}>Let's get baking!</div>
+    <p className={styles.Message}>
+      Add your favourite recipes through the <nobr>Add Recipe button 
+      <IconButton icon='library_add' className={styles.Icon} /></nobr> at the top of the 
+      screen.
+    </p>
+    <p className={styles.Message}>
+      When you add your recipes, a shopping list will be automatically
+      generated. Just use the <nobr>Basket button 
+      <IconButton icon='shopping_basket' className={styles.Icon} /></nobr> to see it.
+    </p>
+    <div className={styles.Illustration}>
+      <img src={bakingIllustration} alt="" />
+    </div>
+  </div>;
+};
+
+export {
+  EmptyState
+};
+export default EmptyState;
